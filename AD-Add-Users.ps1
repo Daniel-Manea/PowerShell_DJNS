4317d96127c93ee1b13666f1b8e0373e395bbeab
A. Daniel Manea <76233860+Daniel-Manea@users.noreply.github.com>
2022-05-04 16:32:52 +0200

    GUI
    Update GUI.

---
+++ AD-Add-Users.ps1
@@ -0,0 +1,92 @@
+﻿function New-DJUsers {
+[CmdletBinding()]
+param (
+   # [Parameter(Mandatory)]
+    #[string] $Path
+)
+
+try { 
+    
+    $mockData = Get-Content -Path C:\Users\Niro\Downloads\MOCK_DATA.json | ConvertFrom-Json
+    
+
+    
+    for ($i = 0; $i -lt $mockData.count; $i++) {  
+        $vari = "" 
+
+        $surnameLenght = $mockData[$i].Surname.Split("").length
+
+        for ($a = 0; $a -lt $mockData[$i].Surname.Split("").length -1; $a++) {
+
+         
+            $surName = $mockData[$i].Surname.Split("")[$a][0];
+            $vari = $($vari + $surName)   
+              
+        }
+
+        $surName = $($vari +  $mockData[$i].Surname.Split("")[-1])
+                   Write-Host $surName     
+          
+        $userName = '{0}{1}' -f $mockData[$i].GivenName.Substring(0, 1), $surName
+        
+            $j = 2
+            while ((Get-ADUser -Filter "samAccountName -eq '$userName'") -and ($userName -notlike "$mockData[$i].GivenName")) {
+            Write-Warning -Message "The username [$($userName)] is already in use. Trying another.."
+            $userName = '{0}{1}' -f $mockData[$i].GivenName.substring(0, $j), $surName
+            Start-sleep -seconds 1
+            $j++
+            
+       
+    }
+
+
+
+
+    $passWord = $mockData[$i].AccountPassword
+    $secPassWord = ConvertTo-SecureString -String $passWord -AsPlainText -Force
+
+    $newUserParams = @{     
+    Name                  = $userName  
+    GivenName             = $mockData[$i].GivenName          
+    Surname               = $mockData[$i].Surname
+    AccountPassword       = $secPassWord    
+    Enabled               = [boolean]$mockData[$i].Enabled
+    Department            = $mockData[$i].Department
+    Title                 = $mockData[$i].Title
+    }
+
+    New-ADUser @newUserParams
+
+    Set-ADUser -Identity $userName -Replace @{subDepartment="director"}
+
+    if (!$mockData[$i].SubDepartment) {
+        Set-ADUser -Identity $userName -Replace @{subDepartment = $mockData[$i].Department}
+    } else {
+        Set-ADUser -Identity $userName -Replace @{subDepartment = $mockData[$i].SubDepartment}
+    }
+    
+    $department = $mockData[$i].Department
+
+   if (-not ($ou = Get-ADOrganizationalUnit -Filter "Name -eq '$department'")) {
+        New-ADOrganizationalUnit -Name "$department" -ProtectedFromAccidentalDeletion $False
+        Move-ADObject -Identity "CN = $userName, CN = Users, DC = DJNS, DC = local" -TargetPath "OU = $department, DC = DJNS, DC = local" 
+    } 
+    else {
+        Move-ADObject -Identity "CN = $userName, CN = Users, DC = DJNS, DC = local" -TargetPath "OU = $department, DC = DJNS, DC = local"
+    }
+
+
+       if (-not ($grp = Get-ADGroup -Filter "Name -eq '$department'")) {
+        New-ADGroup -Name "$department" -GroupScope Global
+        Add-ADGroupMember -Identity $department -Members $userName 
+    }
+    else {
+        Add-ADGroupMember -Identity $department -Members $userName 
+    }
+
+    } 
+    
+} catch {
+    Write-Error -Message $_.Exception.Message -Verbose
+}
+} New-DJUsers
