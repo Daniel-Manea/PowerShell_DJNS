d548c2d6e6263e09ab2904c14471cce649838c08
A. Daniel Manea <76233860+Daniel-Manea@users.noreply.github.com>
2022-05-09 14:00:35 +0200

    Add files via upload

--- userCreation.ps1
+++ userCreation.ps1
@@ -1,4 +1,4 @@
-function New-DJUsers {
+﻿function New-DJUsers {
 [CmdletBinding()]
 param (
    # [Parameter(Mandatory)]
@@ -34,12 +34,8 @@ try {
             $userName = '{0}{1}' -f $mockData[$i].GivenName.substring(0, $j), $surName
             Start-sleep -seconds 1
             $j++
-            
-       
+                
     }
-
-
-
 
     $passWord = $mockData[$i].AccountPassword
     $secPassWord = ConvertTo-SecureString -String $passWord -AsPlainText -Force
@@ -63,27 +59,80 @@ try {
     } else {
         Set-ADUser -Identity $userName -Replace @{subDepartment = $mockData[$i].SubDepartment}
     }
+
+
+    Set-ADUser -Identity $userName -Replace @{section ="director"}
+
+    if (!$mockData[$i].Section) {
+        Set-ADUser -Identity $userName -Replace @{section = $mockData[$i].Department}
+    } else {
+        Set-ADUser -Identity $userName -Replace @{section = $mockData[$i].Section}
+    }
     
     $department = $mockData[$i].Department
 
-   if (-not ($ou = Get-ADOrganizationalUnit -Filter "Name -eq '$department'")) {
+    $subDepartment = $mockData[$i].SubDepartment
+    write-host $subDepartment
+
+    $section = $mockData[$i].Section
+
+    if (-not ($ou = Get-ADOrganizationalUnit -Filter "Name -eq '$department'")) {
         New-ADOrganizationalUnit -Name "$department" -ProtectedFromAccidentalDeletion $False
         Move-ADObject -Identity "CN = $userName, CN = Users, DC = DJNS, DC = local" -TargetPath "OU = $department, DC = DJNS, DC = local" 
-    } 
-    else {
+    } else {
         Move-ADObject -Identity "CN = $userName, CN = Users, DC = DJNS, DC = local" -TargetPath "OU = $department, DC = DJNS, DC = local"
     }
 
-
-       if (-not ($grp = Get-ADGroup -Filter "Name -eq '$department'")) {
+   if (-not ($grp = Get-ADGroup -Filter "Name -eq '$department'")) {
         New-ADGroup -Name "$department" -GroupScope Global
         Add-ADGroupMember -Identity $department -Members $userName 
-    }
-    else {
+    } else {
         Add-ADGroupMember -Identity $department -Members $userName 
     }
+      
+    if (-not ($ou = Get-ADOrganizationalUnit -Filter "Name -eq '$subDepartment'")) {
+        New-ADOrganizationalUnit -Name "$subDepartment" -ProtectedFromAccidentalDeletion $False
+        Move-ADObject -Identity "CN = $userName, OU = $department, DC = DJNS, DC = local" -TargetPath "OU = $subDepartment, DC = DJNS, DC = local"
+        Move-ADObject -Identity "OU = $subDepartment, DC = DJNS, DC = local" -TargetPath "OU = $department, DC = DJNS, DC = local" 
+    } else {
+       
+        Try {
+        Move-ADObject -Identity "CN = $userName, OU = $department, DC = DJNS, DC = local" -TargetPath "OU = $subDepartment, OU = $department, DC = DJNS, DC = local"
+        Move-ADObject -Identity "OU = $subDepartment, DC = DJNS, DC = local" -TargetPath "OU = $department, DC = DJNS, DC = local"
+        }
+        Catch{}
+    }
+  
+    if (-not ($grp = Get-ADGroup -Filter "Name -eq '$subDepartment'")) {
+        New-ADGroup -Name "$subDepartment" -GroupScope Global
+        Add-ADGroupMember -Identity $subDepartment -Members $userName 
+    } else {
+        Add-ADGroupMember -Identity $subDepartment -Members $userName 
+    }
+    
+    if (-not ($ou = Get-ADOrganizationalUnit -Filter "Name -eq '$section'")) {
+        New-ADOrganizationalUnit -Name "$section" -ProtectedFromAccidentalDeletion $False
+        Move-ADObject -Identity "CN = $userName, OU = $subDepartment, OU = $department, DC = DJNS, DC = local" -TargetPath "OU = $section, DC = DJNS, DC = local"
+        Try{
+        Move-ADObject -Identity "OU = $section, DC = DJNS, DC = local" -TargetPath "OU = $subDepartment, OU = $department, DC = DJNS, DC = local"
+        }
+        Catch{}
+    } else {
+        Try {
+        Move-ADObject -Identity "CN = $userName, OU = $subDepartment, OU = $department, DC = DJNS, DC = local" -TargetPath "OU = $section, OU = $subDepartment, OU = $department, DC = DJNS, DC = local"
+        Move-ADObject -Identity "OU = $section, DC = DJNS, DC = local" -TargetPath "OU = $subDepartment, OU = $department, DC = DJNS, DC = local" 
+        }
+        Catch{}
+    }
 
-    } 
+    if (-not ($grp = Get-ADGroup -Filter "Name -eq '$section'")) {
+        New-ADGroup -Name "$section" -GroupScope Global
+        Add-ADGroupMember -Identity $section -Members $userName 
+    } else {
+        Add-ADGroupMember -Identity $section -Members $userName 
+   }
+  
+    }
     
 } catch {
     Write-Error -Message $_.Exception.Message -Verbose
